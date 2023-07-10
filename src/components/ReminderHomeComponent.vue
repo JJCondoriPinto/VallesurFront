62b3463c421148a18b38b1d125fa260bc86bc601
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-07-10 16:55:40 -0500

    home gerente

--- src/components/ReminderHomeComponent.vue
+++ src/components/ReminderHomeComponent.vue
@@ -31,23 +31,23 @@
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="newRemindLabel">Nuevo recordatorio</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="minRemind()"></button>
       </div>
       <div class="modal-body">
         <form>
           <div class="mb-3">
             <label for="remind-name" class="col-form-label">Titulo:</label>
-            <input type="text" class="form-control" id="remind-name">
+            <input type="text" class="form-control" id="remind-name" v-model="this.reminds[this.reminds.length-1].titulo">
           </div>
           <div class="mb-3">
             <label for="description-text" class="col-form-label">Description:</label>
-            <textarea class="form-control" id="description-text"></textarea>
+            <textarea class="form-control" id="description-text" v-model="this.reminds[this.reminds.length-1].descripcion"></textarea>
           </div>
         </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Generar recordatorio</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="minRemind()">Cerrar</button>
+        <button type="button" class="btn btn-primary" @click="insertRemind()" data-bs-dismiss="modal">Generar recordatorio</button>
       </div>
     </div>
   </div>
@@ -61,7 +61,10 @@
         name: 'RemindersHomeComponent',
         data() {
             return {
-                reminds: []
+                reminds: [{
+                    titulo: "",
+                    descripcion: "",
+                }]
             }
         },
         created() {
@@ -76,6 +79,12 @@
                     titulo: "",
                     descripcion: ""
                 });
+            },
+            insertRemind() {
+                axios.post('api/home/remind', this.reminds[this.reminds.length-1])
+            },
+            minRemind() {
+                this.reminds.pop();
             },
             deleteRemind(id) {
                 axios.delete(`api/home/remind/${id}`);
